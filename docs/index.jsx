a8806b3aa65d2c18d3118911f3d1277afe6fe405
Daniel de la Cruz <daniel.delacruz@scmspain.com>
2015-10-28 12:35:40 +0100

    changed data model

--- docs/index.jsx
+++ docs/index.jsx
@@ -2,39 +2,7 @@ import React from 'react';
 import Footer from '../src';
 import './style.scss';
 import '../src/index.scss';
-
-const data = [
-    {
-      block: 'cats',
-      items: [
-        {literal: 'Persian Cat', link: 'https://en.wikipedia.org/wiki/Persian_cat'},
-        {literal: 'Balinese Cat', link: 'https://en.wikipedia.org/wiki/Balinese_cat'},
-        {literal: 'British Shorthair Cat', link: 'https://es.wikipedia.org/wiki/British_Shorthair'},
-        {literal: 'British Shorthair Cat', link: 'https://es.wikipedia.org/wiki/British_Shorthair'},
-        {literal: 'British Shorthair Cat', link: 'https://es.wikipedia.org/wiki/British_Shorthair'},
-        {literal: 'British Shorthair Cat', link: 'https://es.wikipedia.org/wiki/British_Shorthair'}
-      ]
-    },
-
-    {
-      block: 'dogs',
-      items: [
-        {literal: 'Border Collie', link: 'https://es.wikipedia.org/wiki/Border_collie'},
-        {literal: 'Dalmatian', link: 'https://en.wikipedia.org/wiki/Dalmatian_(dog)'},
-        {literal: 'Shetland Sheepdog', link: 'https://en.wikipedia.org/wiki/Shetland_Sheepdog'}
-      ]
-    }
-  ,
-
-    {
-      block: 'hamsters',
-      items: [
-        {literal: 'Syrian Hamsters', link: 'https://en.wikipedia.org/wiki/Golden_hamster'},
-        {literal: 'Dwarf Campbell', link: 'https://en.wikipedia.org/wiki/Campbell%27s_dwarf_hamster'},
-        {literal: 'Phodopus roborovskii', link: 'https://es.wikipedia.org/wiki/Phodopus_roborovskii'}
-      ]
-    }
-];
+import data from './data';
 
 React.render(
   <Footer data={data} />,
